5f60cccfaed71ded25ccbf961b16d2e4b1db7102
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-12-01 10:57:01 +0100

    Add seeders generated with iseed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            ReferenceSeeder::class,
-        ]);
+        $this->call(OpinionsTableSeeder::class);
+        $this->call(OpinionstatesTableSeeder::class);
+        $this->call(ReferencesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+        $this->call(ThemesTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(OpinionstatetransitionsTableSeeder::class);
+        $this->call(OpinionsHasReferencesTableSeeder::class);
+        $this->call(UsersCommentOpinionsTableSeeder::class);
     }
 }
