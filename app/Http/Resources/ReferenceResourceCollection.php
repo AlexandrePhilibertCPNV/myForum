71488418d51629b389d20cdabde48055f51fd5a5
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-12-08 11:01:47 +0100

    Rename references ressources

--- app/Http/Resources/ReferenceResourceCollection.php
+++ app/Http/Resources/ReferenceResourceCollection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ReferenceRessourceCollection extends ResourceCollection
+class ReferenceResourceCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
