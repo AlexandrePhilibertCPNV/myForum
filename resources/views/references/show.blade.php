6949d265481e3b1d405c16383e9e7a2550dc3b3d
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-12-08 08:48:09 +0100

    Add modal window to confirm delete action of ressource

--- resources/views/references/show.blade.php
+++ resources/views/references/show.blade.php
@@ -18,9 +18,30 @@
             <td>{{ $reference->url }}</td>
             <td>
                 <a class="btn btn-secondary" href="{{ route('references.edit', $reference->id) }}">Edit</a>
-                <a class="btn btn-primary" href="{{ route('references.destroy', $reference->id) }}" data-confirm="Êtes-vous certains de vouloir supprimer cette référence ?">Delete</a>
+                <button class="btn btn-primary" data-toggle="modal" data-target="#deleteModal">Delete</button>
             </td>
         </tr>
     </tbody>
 </table>
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="deleteModalLabel">Confirmation de suppression</h5>
+          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Êtes-vous certains de vouloir supprimer cette référence ?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <form action="{{ route('references.destroy',$reference->id) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-primary">Delete</button>
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
