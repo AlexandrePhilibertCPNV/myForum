89b4a3d47d729edcf04612778f532a1e54c99876
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-12-08 08:28:52 +0100

    Add CRUD methods to reference controller

--- app/Http/Controllers/ReferenceController.php
+++ app/Http/Controllers/ReferenceController.php
@@ -14,16 +14,47 @@ class ReferenceController extends Controller
         return view('references.index')->with('references', $references);
     }
 
-    public function show($id)
+    public function store(Request $request)
+    {
+        $reference = new Reference();
+        $reference->description = $request->input('description');
+        $reference->url = $request->input('url');
+        $reference->save();
+        return redirect(route('references.index'));
+    }
+
+
+    public function show(int $id)
     {
         $reference = Reference::find($id);
         return view('references.show')->with('reference', $reference);
     }
 
-    public function destroy($id)
+    public function create()
+    {
+        $reference = new Reference();
+        return view ('references.create')->with('reference', $reference);
+    }
+
+    public function edit(int $id)
+    {
+        $reference = Reference::find($id);
+        return view('references.edit')->with('reference', $reference);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $reference = Reference::find($id);
+        $reference->description = $request->input('description');
+        $reference->url = $request->input('url');
+        $reference->save();
+        return view('references.show')->with('reference', $reference);
+    }
+
+    public function destroy(int $id)
     {
         Reference::destroy($id);
 
-        return redirect('/references');
+        return redirect(route('references.index'));
     }
 }
